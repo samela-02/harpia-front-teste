591992cca49d252797408bd0edd03124d873bbdd
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-06-05 19:48:18 -0300

    feat: adiciona validators do documento e do renavam

--- src/app/presentation/views/modules/gerenciamento/lista-alertas-page/alertas-page/components/alerta/page-create-alerta/page-create-alerta.component.ts
+++ src/app/presentation/views/modules/gerenciamento/lista-alertas-page/alertas-page/components/alerta/page-create-alerta/page-create-alerta.component.ts
@@ -66,7 +66,7 @@ export class ModalFormCreateAlertaComponent {
       nmMarca: [''],
       nrAno: [],
       nrChassi: ['', Validators.pattern("^(?=.*[A-HJ-NPR-Z])(?=.*[0-9])[A-HJ-NPR-Z0-9]{17}$")],
-      nrRenavam: ['', Validators.pattern("^\d{11}$")],
+      nrRenavam: ['', Validators.pattern("^[0-9]{9,11}$")],
       endereco: this.formBuilder.group({
         dsLogradouro: [''],
         nrEndereco: [''],
@@ -77,7 +77,7 @@ export class ModalFormCreateAlertaComponent {
       }),
       proprietario: this.formBuilder.group({
         nmProprietario: [''],
-        nrDocumento: ['']
+        nrDocumento: ['', Validators.maxLength(20)]
       })
     }) as FormGroup<FormType<Veiculo>>
   }
