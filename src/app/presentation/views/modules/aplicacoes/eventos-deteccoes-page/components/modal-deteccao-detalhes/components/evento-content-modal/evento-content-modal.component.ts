6d14e9a9313c5a0d22c14dee0e838987f11f9a91
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-06-05 16:46:35 -0300

    feat: adiciona pin no mapa

--- src/app/presentation/views/modules/aplicacoes/eventos-deteccoes-page/components/modal-deteccao-detalhes/components/evento-content-modal/evento-content-modal.component.ts
+++ src/app/presentation/views/modules/aplicacoes/eventos-deteccoes-page/components/modal-deteccao-detalhes/components/evento-content-modal/evento-content-modal.component.ts
@@ -4,12 +4,12 @@ import { InfoLineComponent } from '@/presentation/shared/components/info-line/in
 import { MapMarkersComponent } from '@/presentation/shared/components/map-markers/map-markers.component';
 import { SetColorByNivel } from '@/presentation/shared/helpers/set-color-by-nivel.helper';
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
+import { AfterViewInit, Component, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { ImagemDeteccaoModalComponent } from './components/imagem-deteccao-modal/imagem-deteccao-modal.component';
 import { ModalService } from '@/infrastructure/services/modal/modal.service';
 import { Store } from '@ngxs/store';
 import { AlertaSelectors } from '@/infrastructure/store/selectors/alerta.selectors';
-import { AlertaCompletoQueryResponse } from '@/domain/models/query/alerta-completo-query-reponse';
+import L from 'leaflet';
 
 @Component({
   selector: 'app-evento-content-modal',
@@ -18,12 +18,18 @@ import { AlertaCompletoQueryResponse } from '@/domain/models/query/alerta-comple
   templateUrl: './evento-content-modal.component.html',
   styleUrl: './evento-content-modal.component.scss'
 })
-export class EventoContentModalComponent {
+export class EventoContentModalComponent implements AfterViewInit {
+  @ViewChild('mapDeteccao') mapComponent!: MapMarkersComponent;
+  
   private _store = inject(Store);
   private _modalService = inject(ModalService<ImagemDeteccaoModalComponent>)
   public dadosAlerta = this._store.selectSignal(AlertaSelectors.alertaPorCd);
 
   @Input() deteccao: DeteccaoQueryResponse;
+
+  ngAfterViewInit(): void {
+    this.plotarPinNoMapa();
+  }
 
   public getColorByNivel(nivel: number): string {
     return SetColorByNivel.setColor(nivel);
@@ -33,5 +39,29 @@ export class EventoContentModalComponent {
     event.stopPropagation();
     event.preventDefault();
     this._modalService.component(ImagemDeteccaoModalComponent).open(deteccao);
+  }
+
+  private plotarPinNoMapa() {
+    const map = this.mapComponent?.getMap();
+    if (map) {
+      const coordenadas: [number, number] = this.getCoordenadas();
+      L.marker(coordenadas, {
+        icon: this.createIcon()
+      }).addTo(map);
+    }
+  }
+
+  private getCoordenadas(): [number, number] {
+    return [this.deteccao.vlLatitude, this.deteccao.vlLongitude];
+  }
+
+  private createIcon(): L.Icon<L.IconOptions> | L.DivIcon {
+    return L.divIcon({
+      html: `<div class="car-icon-wrapper">
+               <img src="assets/pin-deteccao.png" class="w-[35px] h-[35px]"/>
+             </div>`,
+      className: '',
+      iconAnchor: [0, 0]
+    });
   }
 }
