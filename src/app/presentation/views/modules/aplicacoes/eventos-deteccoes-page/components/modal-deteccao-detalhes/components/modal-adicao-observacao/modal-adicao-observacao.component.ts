69d52d9f5c39c1349befe62a49a960cd2ae6195a
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-06-16 15:57:00 -0300

    feat: implementa o modal e form da observação

--- src/app/presentation/views/modules/aplicacoes/eventos-deteccoes-page/components/modal-deteccao-detalhes/components/modal-adicao-observacao/modal-adicao-observacao.component.ts
+++ src/app/presentation/views/modules/aplicacoes/eventos-deteccoes-page/components/modal-deteccao-detalhes/components/modal-adicao-observacao/modal-adicao-observacao.component.ts
@@ -1,14 +1,21 @@
 import { DeteccaoQueryResponse } from '@/domain/models/query/deteccao-query-response';
 import { Component, inject } from '@angular/core';
-import { MODAL_DATA } from '@tivic-team/tivic-ui';
+import { ButtonComponent } from '@tivic-team/tivic-ui';
+import { FormAdicaoObservacaoComponent } from "./form-adicao-observacao/form-adicao-observacao.component";
+import { MODAL_DATA, ModalService } from '@/infrastructure/services/modal/modal.service';
 
 @Component({
   selector: 'app-modal-adicao-observacao',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, FormAdicaoObservacaoComponent],
   templateUrl: './modal-adicao-observacao.component.html',
   styleUrl: './modal-adicao-observacao.component.scss'
 })
 export class ModalAdicaoObservacaoComponent {
   protected deteccao: DeteccaoQueryResponse = inject(MODAL_DATA) as DeteccaoQueryResponse;
+  private _modalServiceAdicaoObservacao = inject(ModalService<ModalAdicaoObservacaoComponent>);
+
+  fecharModal(): void {
+    this._modalServiceAdicaoObservacao.component(ModalAdicaoObservacaoComponent).dismiss(this.deteccao);
+  }
 }
