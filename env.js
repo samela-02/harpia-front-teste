484f3ec0dc7e9983a1206910a2e2357311dc47fb
samela-02 <101211846+samela-02@users.noreply.github.com>
2025-07-01 14:58:52 -0300

    Create env.js

---
+++ env.js
@@ -0,0 +1,9 @@
+(function (window) {
+    window.env = window.env || {};
+
+    window["env"]["protocol"]    = "http";
+    window["env"]["apiroot"]     = "v1";
+    window["env"]["host"]        = "192.168.1.130";
+    window["env"]["port"]        = "8091";
+    window["env"]["context"]     = "api";
+})(this);
