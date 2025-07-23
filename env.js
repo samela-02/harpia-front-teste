1482f50bd030cadd6ade407e42d0511045e61621
samela-02 <101211846+samela-02@users.noreply.github.com>
2025-07-23 14:00:56 -0300

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    window.env = window.env || {};
-
-    window["env"]["protocol"]    = "http";
-    window["env"]["apiroot"]     = "v1";
-    window["env"]["host"]        = "192.168.1.130";
-    window["env"]["port"]        = "8091";
-    window["env"]["context"]     = "api";
-})(this);
