afb589610d2b897cc34bf96a84aec4f4d724d0ea
samela-02 <101211846+samela-02@users.noreply.github.com>
2025-08-11 15:36:08 -0300

    Update env.js

--- env.js
+++ env.js
@@ -3,7 +3,7 @@
 
     window["env"]["protocol"]    = "http";
     window["env"]["apiroot"]     = "v1";
-    window["env"]["host"]        = "192.168.1.130";
+    window["env"]["host"]        = "localhost";
     window["env"]["port"]        = "8091";
     window["env"]["context"]     = "api";
 })(this);
